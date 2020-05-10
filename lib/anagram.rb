97d36eaaee89d5057bc902d905dea51875d743e4
Kyle McLean <kyledmclean@gmail.com>
2020-05-10 03:56:55 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,8 +11,8 @@ class Anagram
   def match(words)
   #  warray = %w(words)
     words.each do |test_word|
-      testarray = test_word.split
-      basearray = word.split
+      testarray = test_word.split("")
+      basearray = word.split("")
       if basearray.sort == testarray.sort
         return test_word
       end
